a1a47892e3770016c36043d9ece07a7facec39e6
hcj <1943791076@qq.com>
2018-09-25 18:37:32 +0800

    项目初始化

---
+++ src/main/java/com/yc/NewvoteApplication.java
@@ -0,0 +1,12 @@
+package com.yc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewvoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewvoteApplication.class, args);
+	}
+}
